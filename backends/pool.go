748ced805c0515c04a325d04e9522d31235ea210
Mohan Dutt <mohandutt134@gmail.com>
2016-06-20 23:00:04 +0530

    changing redis library from redigo to go-redis/redisV3

--- backends/pool.go
+++ backends/pool.go
@@ -5,49 +5,72 @@ import (
 	"sync"
 	"time"
 
-	"github.com/garyburd/redigo/redis"
+	"gopkg.in/redis.v3"
 )
 
-func newPool(server, password string) *redis.Pool {
+func newClient(server, password string) *redis.Client {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
-	return &redis.Pool{
-		MaxIdle:     3,
+	options := &redis.Options{
+		Network: "tcp",
+		Addr:    server,
+
+		// Dialer creates new network connection and has priority over
+		// Network and Addr options.
+		// Dialer func() (net.Conn, error)
+
+		// An optional password. Must match the password specified in the
+		// requirepass server configuration option.
+		Password: password,
+		// A database to be selected after connecting to server.
+		DB: 0,
+
+		// The maximum number of retries before giving up.
+		// Default is to not retry failed commands.
+		// MaxRetries int
+
+		// Sets the deadline for establishing new connections. If reached,
+		// dial will fail with a timeout.
+		// Default is 5 seconds.
+		// DialTimeout time.Duration
+		// Sets the deadline for socket reads. If reached, commands will
+		// fail with a timeout instead of blocking.
+		// ReadTimeout time.Duration
+		// Sets the deadline for socket writes. If reached, commands will
+		// fail with a timeout instead of blocking.
+		// WriteTimeout time.Duration
+
+		// The maximum number of socket connections.
+		// Default is 10 connections.
+		PoolSize: 1000,
+		// Specifies amount of time client waits for connection if all
+		// connections are busy before returning an error.
+		// Default is 1 second.
+		// PoolTimeout time.Duration
+		// Specifies amount of time after which client closes idle
+		// connections. Should be less than server's timeout.
+		// Default is to not close idle connections.
 		IdleTimeout: 240 * time.Second,
-		Dial: func() (redis.Conn, error) {
-			c, err := redis.Dial("tcp", server)
-			if err != nil {
-				return nil, err
-			}
-
-			if password != "" {
-				if _, err := c.Do("AUTH", password); err != nil {
-					c.Close()
-					return nil, err
-				}
-			}
-
-			return c, err
-		},
-		TestOnBorrow: func(c redis.Conn, t time.Time) error {
-			_, err := c.Do("PING")
-			return err
-		},
+		// The frequency of idle checks.
+		// Default is 1 minute.
+		// IdleCheckFrequency time.Duration
 	}
+
+	return redis.NewClient(options)
 }
 
 var cached = struct {
 	sync.RWMutex
-	pool *redis.Pool
+	client *redis.Client
 }{}
 
-func getPool(redis_host, password string) *redis.Pool {
+func getClient(redis_host, password string) *redis.Client {
 
 	cached.Lock()
-	if cached.pool == nil {
-		cached.pool = newPool(redis_host, password)
+	if cached.client == nil {
+		cached.client = newClient(redis_host, password)
 	}
 	cached.Unlock()
 
-	return cached.pool
+	return cached.client
 }
